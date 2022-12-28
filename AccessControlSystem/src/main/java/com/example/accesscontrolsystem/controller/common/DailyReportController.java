f993c74085c2543b8a277fe123af7d7b9eba076f
zhoudazhi <dazhi527@hotmail.com>
2022-12-28 21:50:24 +0800

    重构controller

--- AccessControlSystem/src/main/java/com/example/accesscontrolsystem/controller/common/DailyReportController.java
+++ AccessControlSystem/src/main/java/com/example/accesscontrolsystem/controller/common/DailyReportController.java
@@ -1,26 +1,26 @@
-package com.example.accesscontrolsystem.controller;
+package com.example.accesscontrolsystem.controller.common;
 
 import com.example.accesscontrolsystem.model.entity.reportNlog.DailyReport;
 import com.example.accesscontrolsystem.service.student.DailyReportService;
 import com.example.accesscontrolsystem.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
 @RestController("StudentDailyReportController")
-@RequestMapping("/student/daily-report")
+@RequestMapping("/daily-report")
 public class DailyReportController {
     private final DailyReportService dailyReportService;
     @Autowired
     public DailyReportController(DailyReportService dailyReportService) {
         this.dailyReportService = dailyReportService;
     }
-    @PostMapping("/")
-    public Response<DailyReport> addDailyReports(@RequestBody DailyReport dailyReport) {
-        return dailyReportService.addDailyReport(dailyReport);
-    }
+
     @GetMapping("/")
     public Response<List<DailyReport>> getDailyReports(@RequestParam("student-id") Integer studentId) {
         return dailyReportService.getDailyReports(studentId);
---
+++ AccessControlSystem/src/main/java/com/example/accesscontrolsystem/controller/student/StudentDailyReportController.java
@@ -0,0 +1,22 @@
+package com.example.accesscontrolsystem.controller.student;
+
+import com.example.accesscontrolsystem.model.entity.reportNlog.DailyReport;
+import com.example.accesscontrolsystem.model.vo.RawDailyReport;
+import com.example.accesscontrolsystem.service.student.DailyReportService;
+import com.example.accesscontrolsystem.util.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("StudentDailyReportController")
+@RequestMapping("/student/daily-report")
+public class StudentDailyReportController {
+    private final DailyReportService dailyReportService;
+    @Autowired
+    public StudentDailyReportController(DailyReportService dailyReportService) {
+        this.dailyReportService = dailyReportService;
+    }
+    @PostMapping("/")
+    public Response<DailyReport> addDailyReports(@RequestBody RawDailyReport dailyReport) {
+        return dailyReportService.addDailyReport(dailyReport);
+    }
+}
