c10395f6bdb263767650b2417d2286e2a3065525
zhoudazhi <dazhi527@hotmail.com>
2022-12-29 16:13:02 +0800

    edit 离校时间一年内

--- AccessControlSystem/src/main/java/com/example/accesscontrolsystem/manager/GateLogManager.java
+++ AccessControlSystem/src/main/java/com/example/accesscontrolsystem/manager/GateLogManager.java
@@ -2,17 +2,28 @@ package com.example.accesscontrolsystem.manager;
 
 import com.example.accesscontrolsystem.mapper.GateLogMapper;
 import com.example.accesscontrolsystem.model.entity.reportNlog.GateLog;
+import com.example.accesscontrolsystem.service.system.TimeService;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component("GateLogManager")
 public class GateLogManager {
     private final GateLogMapper gateLogMapper;
+    private final TimeService timeService;
 
-    public GateLogManager(GateLogMapper gateLogMapper) {
+    public GateLogManager(GateLogMapper gateLogMapper, TimeService timeService) {
         this.gateLogMapper = gateLogMapper;
+        this.timeService = timeService;
     }
 
     public void create(GateLog gateLog) {
         gateLogMapper.save(gateLog);
+    }
+
+    public List<GateLog> getOneYearGateLogsByStudentId(Integer studentId) {
+        Long today = timeService.getTime();
+        Long nDaysBefore = timeService.getTimeNDaysBefore(365);
+        return gateLogMapper.findGateLogsByStudentIdAndTimeBetween(studentId, nDaysBefore, today);
     }
 }
--- AccessControlSystem/src/main/java/com/example/accesscontrolsystem/mapper/GateLogMapper.java
+++ AccessControlSystem/src/main/java/com/example/accesscontrolsystem/mapper/GateLogMapper.java
@@ -2,6 +2,7 @@ package com.example.accesscontrolsystem.mapper;
 
 import com.example.accesscontrolsystem.model.entity.reportNlog.GateLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface GateLogMapper extends JpaRepository<GateLog, Integer> {
     List<GateLog> findAllByStudentId(Integer studentId);
     GateLog findGateLogById(Integer id);
+
+    @Query("select g from GateLog g where g.student.id = ?1 and g.time between ?2 and ?3")
+    List<GateLog> findGateLogsByStudentIdAndTimeBetween(Integer studentId, Long nDaysBefore, Long today);
 }
