28a8eb9371e975f086a318522b8cba4e121881bf
zhoudazhi <dazhi527@hotmail.com>
2022-12-31 10:49:44 +0800

    add id查用户

--- AccessControlSystem/src/main/java/com/example/accesscontrolsystem/controller/userManagement/CounsellorController.java
+++ AccessControlSystem/src/main/java/com/example/accesscontrolsystem/controller/userManagement/CounsellorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController("SuperUserCounsellorController")
-@RequestMapping("/super-user/counsellor")
+@RequestMapping("/counsellor")
 public class CounsellorController {
     private final UserService userservice;
     @Autowired
--- AccessControlSystem/src/main/java/com/example/accesscontrolsystem/controller/userManagement/SchoolManagerController.java
+++ AccessControlSystem/src/main/java/com/example/accesscontrolsystem/controller/userManagement/SchoolManagerController.java
@@ -6,10 +6,12 @@ import com.example.accesscontrolsystem.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController("SchoolManagerController")
+@RequestMapping("/school-manager")
 public class SchoolManagerController {
     private final UserService userservice;
     @Autowired
